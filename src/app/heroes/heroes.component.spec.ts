f6a247712f147a241b8a472eb02ff910ab436f15
Junior <junior.jaava@gmail.com>
2021-02-01 19:25:18 +0100

    3.4 Isolating test: testing component

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,15 @@
+
+import { HeroesComponent } from './heroes.component'
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+
+  beforeEach(() => {
+    HEROES = [
+      {id:1, name:'SpiderDude', }
+
+    ]
+     
+  })
+})
