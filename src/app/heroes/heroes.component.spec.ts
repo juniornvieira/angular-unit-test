a6b407f9d04256a7ae6955bf202d84792faab897
Junior <junior.jaava@gmail.com>
2021-02-01 19:54:44 +0100

    3.5 Isolating test: mocking HeroService

--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -4,12 +4,16 @@ import { HeroesComponent } from './heroes.component'
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let HEROES;
+  let mockHeroService;
 
   beforeEach(() => {
     HEROES = [
-      {id:1, name:'SpiderDude', }
-
+      {id:1, name:'SpiderDude', strength: 8},
+      {id:1, name:'Wonderful woman', strength: 24},
+      {id:1, name:'SuperrDude', strength: 55}
     ]
-     
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero','deleteHero'])
+    component = new HeroesComponent(mockHeroService);
   })
 })
