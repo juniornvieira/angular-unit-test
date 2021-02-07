894586b77f6690bf7ba7a4e421f713a37aecaa5c
Junior <junior.jaava@gmail.com>
2021-02-07 16:59:21 +0100

    4.6 shallow integration test: debugElement vs nativeElement

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { HeroComponent } from './hero.component';
 
 describe('HeroComponent (Shallow tests)', () => {
@@ -24,7 +25,10 @@ describe('HeroComponent (Shallow tests)', () => {
     fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
     fixture.detectChanges();//reponsible for detected binding changes. Ex.: {{hero.name}}
 
-    expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude');
+    let deA = fixture.debugElement.query(By.css('a'));  //debugElement
+    expect(deA.nativeElement.textContent).toContain('SuperDude');
+    
+    // expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude'); // nativeElement
   });
 
 });
