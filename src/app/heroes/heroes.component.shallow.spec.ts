5a95c848f5a045610c10326365b60b30d7f96c22
Junior <junior.jaava@gmail.com>
2021-02-09 20:29:07 +0100

    4.8 shallow integration test: mocking the service

--- src/app/heroes/heroes.component.shallow.spec.ts
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -1,32 +1,41 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { HeroService } from '../hero.service';
 import { HeroesComponent } from './heroes.component';
 
 describe('HeroesComponent (Shallow tests)', () => {
-  let fixture: ComponentFixture<HeroesComponent>;
+  let fixture: ComponentFixture<HeroesComponent>; //call component
+  let HEROES;
+  let mockHeroService;
 
   beforeEach(() => {
+    HEROES = [
+      {id:1, name:'SpiderDude', strength: 8},
+      {id:1, name:'Wonderful woman', strength: 24},
+      {id:1, name:'SuperrDude', strength: 55}
+    ]
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero','deleteHero'])//mock service
+    
     TestBed.configureTestingModule({
       declarations: [HeroesComponent],
+      providers: [
+        {provide: HeroService, useValue: mockHeroService} //mock service
+      ],
+      schemas: [NO_ERRORS_SCHEMA] 
     });
+
     fixture = TestBed.createComponent(HeroesComponent);
   });
 
-  it('should have the correct heroes ', () => {
-    // fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+  it('should set heroes correctly from service ', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES)); //mock service
+    fixture.detectChanges(); //call component
 
-    // expect(fixture.componentInstance.hero.name).toEqual ('SuperDude');
+    expect(fixture.componentInstance.heroes.length).toBe(3); //call component
   });
 
-  it('should render the hero name in anchor tag ', () => {
-    // fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
-    // fixture.detectChanges();//reponsible for detected binding changes. Ex.: {{hero.name}}
-
-    // let deA = fixture.debugElement.query(By.css('a'));  //debugElement
-    // expect(deA.nativeElement.textContent).toContain('SuperDude');
-    
-    // expect(fixture.nativeElement.querySelector('a').textContent).toContain('SuperDude'); // nativeElement
-  });
+ 
 
 });
