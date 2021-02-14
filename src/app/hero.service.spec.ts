f6aaffc9c5501bb4e50ad86ba2181a1f57964bcc
Junior <junior.jaava@gmail.com>
2021-02-14 19:34:40 +0100

    5.5 deep integration test: + implementing testing with mocked HTTP

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -2,10 +2,12 @@ import {  TestBed } from '@angular/core/testing';
 import { MessageService } from './message.service';
 
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HeroService } from './hero.service';
 
-describe('HeroesComponent (deep tests)', () => {
+describe('HeroService', () => {
   let mockMessageService;
   let httpTestingController: HttpTestingController; //to mock private http: HttpClient
+  let service: HeroService;
 
   beforeEach(() => {
     mockMessageService = jasmine.createSpyObj(['add'])//mock service
@@ -15,11 +17,23 @@ describe('HeroesComponent (deep tests)', () => {
         HttpClientTestingModule
       ],
       providers: [
+          HeroService,
         { provide: MessageService, useValue: mockMessageService } //mock service
       ]
     });
 
     httpTestingController = TestBed.get(HttpTestingController); //to mock private http: HttpClient
+    service = TestBed.get(HeroService);
+  });
 
+  describe('getHero', () => {
+      it('should call get with correct URL', () => {
+        
+        service.getHero(4).subscribe();
+
+        const req = httpTestingController.expectOne('api/heroes/4'); //expectation
+        req.flush({id: 4, name: 'SuperDude', strength: 100}) // set return as an object
+        httpTestingController.verify(); //make sure going to be called once only
+      });
   });
 });
